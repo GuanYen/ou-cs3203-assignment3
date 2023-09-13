bc01c0007cbcddaeaf4283369d5c1c129f4d05e3
547034 <chen0198@gpel8.cs.nor.ou.edu>
2023-09-12 22:14:36 -0500

    Added main file to prompt user and get input for methods.

--- program.c
+++ program.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+int sumOfList(int a[], int s);
+int productOfList(int a[], int s);
 
-int sumOfList(int[] a){
+int sumOfList(int a[], int s){
 	int sum = 0;
-	int size = sizeof(a) / sizeof(a[0]);
+	int size = s;
 
 	for (int i = 0; i < size; ++i)
 	{
@@ -11,13 +13,47 @@ int sumOfList(int[] a){
 	return sum;
 }
 
-int productOfList(int[] a){
+int productOfList(int a[], int s){
 	int product = a[0];
-	int size = sizeof(a) / sizeof(a[0]);
+	int size = s;
 
 	for (int i = 1; i < size; ++i)
 	{
 		product *=  a[i];
 	}
 	return product;
+}
+
+int main(){
+	int num = 0;
+	int size = 0;
+
+	printf("Enter size for array: ");
+	scanf("%d", &size);
+	int list[size];
+
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	list[0] = num;
+	int i = 1;
+
+	while (num != -1)
+	{	
+		printf("Enter a number: ");
+		scanf("%d", &num);
+		list[i] = num;
+		++i;
+
+		if (i == size){
+			printf("There is no more space in the array. \n");
+			num = -1;
+		}
+	}
+
+	int sum = sumOfList(list, size);
+	printf("Sum of List: %d\n", sum);
+
+	int product = productOfList(list, size);
+	printf("Product of List: %d\n", product);
+	return 0;
 }
